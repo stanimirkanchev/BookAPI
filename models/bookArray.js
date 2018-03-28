29a847bc1d16577f0e38d2fe250eee70edfb8ad1
stanimirkanchev <32367896+stanimirkanchev@users.noreply.github.com>
2018-03-28 23:40:08 +0300

    Add files via upload

---
+++ models/bookArray.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+      title: 'Night',
+      year: '1960',
+      author: ' Elie Wiesel',
+      genre: 'autobiography'
+    },
+    {
+      title: 'The Glass Castle ',
+      year: '2005',
+      author: 'Jeannette Walls',
+      genre: 'autobiography'
+    },
+    {
+      title: 'The Autobiography of Malcolm X',
+      year: '1965',
+      author: 'Malcolm X with Alex Haley',
+      genre: 'autobiography'
+    },
+    {
+      title: 'A Tolkien Bestiary',
+      year: '1978',
+      author: 'David Day',
+      genre: 'bestiary'
+    },
+    {
+      title: 'Gnomes',
+      year: '1965',
+      author: 'Wil Huygen',
+      genre: 'bestiary'
+    },
+    {
+      title: 'MyOwn',
+      year: '2000',
+      author: 'Stanimir kanchev',
+      genre: 'Hello'
+    }
+  ];
